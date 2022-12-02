b599dfdece3e0219d8c22a601a5c4df3705c1d2d
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-02 15:47:19 +0100

    two one-liners

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import axios from 'axios';
 
 const url = 'http://localhost:5577/flashcards';
 
-const response = axios.get(url);
+// FETCH ONE-LINER
+// const flashcards = await(await fetch(url)).json();
+
+// AXIOS ONE-LINER
+// const flashcards = (await axios.get(url)).data;
+
+const response = await axios.get(url);
 const flashcards = response.data;
-console.log(flashcards);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h1>Flashcards</h1>
