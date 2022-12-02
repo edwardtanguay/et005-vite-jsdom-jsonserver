4310f2ed6a5de41d0475cbb3e4583483d7e8d010
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-02 15:56:53 +0100

    flashcards styled

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import './style.css';
+import './style.scss';
 import axios from 'axios';
+
+type Flashcard = {
+	id: number,
+	category: string,
+	front: string,
+	back: string;
+}
 
 const url = 'http://localhost:5577/flashcards';
 
@@ -14,4 +21,15 @@ const flashcards = response.data;
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h1>Flashcards</h1>
-`
+<div class="flashcards">
+
+${flashcards.map((flashcard: Flashcard) => {
+	return `<div class="flashcard">
+<div class="category">${flashcard.category}</div>	
+<div class="front">${flashcard.front}</div>	
+<div class="back">${flashcard.back}</div>	
+	</div>`;
+}).join('')}
+
+</div>
+`;
