6695f9471549cd94ba3393092dca24953ec07a8f
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-02 15:33:30 +0100

    flashcard title

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import './style.css'
-import { setupCounter } from './counter'
+import './style.css';
+import axios from 'axios';
+
+const url = 'http://localhost:5577/flashcards';
+
+const response = axios.get(url);
+const flashcards = response.data;
+console.log(flashcards);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  
-  </div>
+<h1>Flashcards</h1>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
